f982b0eccf020a92e26df94a49b4cb686a59b2fb
Wolverinecryptowolfs <141962627+Wolverinecryptowolfs@users.noreply.github.com>
2025-08-29 18:02:36 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,24 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// FILE 10: src/index.css
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-body {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  background: #111827;
-  color: white;
-}
